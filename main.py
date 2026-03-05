fa28fb529e81f69ed8a6b86057267f088ef879cf
praveen_kpm01 <praveenkpm1103@gmail.com>
2026-03-05 18:58:52 +0530

    Day 7 at Besant

--- main.py
+++ main.py
@@ -1,11 +1,32 @@
-print("Hello, World!")
+# print("Hello, World!")
 
-name  = "Praveen"
-exp  = 1
-is_actice = True
-value = 15000.00
+# name  = "Praveen"
+# exp  = 1
+# is_actice = True
+# value = 15000.00
 
-print(type(name))
-print(type(exp))
-print(type(is_actice))
-print(type(value))
+# print(type(name))
+# print(type(exp))
+# print(type(is_actice))
+# print(type(value))
+
+# Arithmatic Operator
+a = int(input("Enter a number : "))
+b = int(input("Enter a number : "))
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a/b)
+# print(a%b)
+# print(a//b)
+# print(a**b)
+
+# Comparison Operators
+'''
+==, !=, >, <, >=, <=
+'''
+
+if a == b :
+    print("A ans B are Equal")
+else:
+    print("A ans B are Not Equal")
